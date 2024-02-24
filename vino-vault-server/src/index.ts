0ad034af3c69df6b574829a46d4aaabc7ceca0cf
DDelvRoj <ddelvallerojas@gmail.com>
2024-02-24 18:10:13 -0300

    Implementación del router para productos.

--- vino-vault-server/src/index.ts
+++ vino-vault-server/src/index.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import cors from 'cors';
 import { authenticateToken } from './middleware/authMiddleware';
-import { apagar, conectar, crearSesionVisor } from './service/cassandraService';
 import loginRouter from './controller/loginController';
 import registroRouter from './controller/registroController';
+import productosRouter from './controller/productosController';
 
 const app = express();
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
@@ -13,21 +13,8 @@ const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.use(express.json());
 app.use(cors());
 app.use(loginRouter);
-app.use(registroRouter)
-// Ruta protegida que requiere autenticación
-app.get('/protected', authenticateToken, async (req: Request, res: Response) => {
-  try {
-    const query = 'SELECT * FROM employees'; // Cambia esto al nombre de tu tabla de empleados
-    const client = crearSesionVisor();
-    await conectar(client);
-    const result = await client.execute(query);
-    await apagar(client);
-    res.json(result.rows);
-  } catch (err) {
-    console.error('Error al obtener empleados desde Cassandra', err);
-    res.status(500).json({ error: 'Hubo un error al procesar tu solicitud' });
-  }
-});
+app.use(registroRouter);
+app.use(productosRouter);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
