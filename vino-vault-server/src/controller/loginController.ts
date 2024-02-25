c863efc39724ce8fd66d7df229241534e13213ba
DDelvRoj <ddelvallerojas@gmail.com>
2024-02-24 23:32:21 -0300

    Implementación de UsuarioService en el respectivo router.

--- vino-vault-server/src/controller/loginController.ts
+++ vino-vault-server/src/controller/loginController.ts
@@ -1,27 +1,19 @@
 import { Router, Request, Response } from "express";
-import { UsuarioModel } from "../model/usuarioModel";
 import { visorSesion } from "../util/sesionUtil";
-import jwt from 'jsonwebtoken';
 import bcryptUtil from "../util/bcryptUtil";
-
-const SECRET_KEY: string | undefined = process.env.CLAVE_FIRMA;
+import { UsuarioService } from "../service/usuarioService";
+import { firmarToken } from "../util/tokenUtil";
 
 const loginRouter = Router();
 
-
 loginRouter.post('/login', async (req: Request, res: Response) => {
     const { username, password } = req.body;
-    const cliente:UsuarioModel = new UsuarioModel(visorSesion);
+    const usuarioBuscador:UsuarioService = new UsuarioService(visorSesion);
     try {
-        if (await !cliente.conectar()){
-            res.status(500).json({ error: 'No se pudo conectar a la base de datos.' });
-            return;
-          };
-        const user = await cliente.obtenerUsuarioPorUsername(username);
-      if (!user) {
+      const user = await usuarioBuscador.buscarUsuarioPorNombre(username);
+      if(!user){
         return res.status(401).json({ error: 'Credenciales inválidas' });
       }
-  
       // Verificar la contraseña
       const passwordMatch = await bcryptUtil.desencriptarYCompararPassword(password, user.password);
       if (!passwordMatch) {
@@ -29,12 +21,12 @@ loginRouter.post('/login', async (req: Request, res: Response) => {
       }
   
       // Generar y enviar el token JWT
-      const token = jwt.sign({ username: user.username }, SECRET_KEY || '');
+      const token = firmarToken({ username: user.username });
       console.log("Inicio de sesión LOGIN éxitoso...");
       res.json({ token });
     } catch (err) {
       res.status(500).json({ error: 'Hubo un error al procesar tu solicitud' });
     }
-    await cliente.desconectar();
   });
+  
 export default loginRouter;
