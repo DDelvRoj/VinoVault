9d7b4eee89961f05072687aec075e1d88e4fa78e
DDelvRoj <ddelvallerojas@gmail.com>
2024-02-29 08:42:59 -0300

    Creación de una utilidad para traducir textos.

---
+++ vino-vault-server/src/util/traductorUtil.ts
@@ -0,0 +1,13 @@
+import { translate } from "@vitalets/google-translate-api";
+
+
+
+export async function traducir(texto:string) {
+    try {
+        const {text} = await translate(texto,{to:'es'});
+        return text;
+    } catch (error) {
+        console.error("No se pudo traducir ",error);
+        return '';
+    }
+}
