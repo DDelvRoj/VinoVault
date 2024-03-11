c28cc27482965f6df5f2c24788eb989579a3ee93
DDelvRoj <ddelvallerojas@gmail.com>
2024-03-11 12:29:26 -0300

    Ajustes de compilación

--- vino-vault-server/src/controller/productosController.ts
+++ vino-vault-server/src/controller/productosController.ts
@@ -1,10 +1,19 @@
 import { Router, Request, Response } from "express";
 import { authenticateToken } from "../middleware/authMiddleware";
+import buscarCodigoDeBarra from "../service/webScrapingService";
 
 const productosRouter = Router();
 
-productosRouter.get('/productos', authenticateToken, async (req: Request, res: Response) => {
-  
+productosRouter.post('/productos', async (req: Request, res: Response) => {
+  try {
+    const {codigo} = req.body;
+    const producto:Producto = await buscarCodigoDeBarra(codigo);
+    res.json(producto);
+  } catch (error) {
+    console.log(error);
+    
+    res.status(500).json({error:'error'})
+  }
 });
 
 export default productosRouter;
