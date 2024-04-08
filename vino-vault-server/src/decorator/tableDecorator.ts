dba564471d2fe648d84e8e36a6d7acdd3742a414
DDelvRoj <ddelvallerojas@gmail.com>
2024-04-08 11:03:31 -0400

    Creación de un Decorator para realizar un Object Manager y evitar el uso de extensión de clases.

---
+++ vino-vault-server/src/decorator/tableDecorator.ts
@@ -0,0 +1,111 @@
+const metadataStorage: WeakMap<any, any> = new WeakMap();
+
+function getMetadata(target: any): any {
+    let metadata = metadataStorage.get(target);
+    if (!metadata) {
+        metadata = {};
+        metadataStorage.set(target, metadata);
+    }
+    return metadata;
+}
+export function Entity(tabla: string): ClassDecorator {
+    return function(target: any) {
+        const tablaNombre = tabla;
+
+        // Métodos para construir consultas SQL aquí
+        Object.defineProperty(target.prototype, 'findAll', {
+            get: function(){
+                const metadata = getMetadata(this.constructor);
+                return `SELECT * FROM ${tablaNombre}`;
+            },
+            configurable: true
+        });
+
+        Object.defineProperty(target.prototype, 'update', {
+            get: function(){
+                const metadata = getMetadata(this.constructor);
+                const columnas = [];
+                for (const prop of metadata.columnas) {
+                    const valor = this[prop];
+                    if (valor !== undefined && valor !== null) {
+                        columnas.push(prop);
+                    }
+                }
+                const ids = metadata.ids;
+                const setValues = columnas.map(col => {
+                    if(!ids.includes(col)){
+                        return `${col}=?`
+                    }
+                }).filter(col=>col!=undefined).join(', ');
+                const whereClause = ids.map(id => `${id}=?`).join(' AND ');
+                return `UPDATE ${tablaNombre} SET ${setValues} WHERE ${whereClause}`;
+            },
+            configurable: true
+        });
+        
+    };
+}
+
+export function Column(params?: { isId?: boolean }): PropertyDecorator {
+    return function(target: any, propertyKey: string) {
+        const className = target.constructor.name;
+        const metadata = getMetadata(target.constructor);
+        metadata.columnas = metadata.columnas || [];
+        metadata.columnas.push(propertyKey.replace(/^_/, ""));
+
+        if (params && params.isId) {
+            metadata.ids = metadata.ids || [];
+            metadata.ids.push(propertyKey);
+        }
+
+        Object.defineProperty(target, propertyKey, {
+            get: function() {
+                return this[`_${propertyKey}`];
+            },
+            set: function(nuevoValor: any) {
+                console.log(`Se ha asignado un nuevo valor "${nuevoValor}" a la columna "${propertyKey}" de la clase "${className}"`);
+                this[`_${propertyKey}`] = nuevoValor;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        Object.defineProperty(target, 'params', {
+            get: function() {
+                const valores = [];
+                for (const prop of metadata.columnas) {
+                    if (!metadata.ids.includes(prop)) {
+                        const valor = this[prop];
+                        if (valor !== undefined && valor !== null) {
+                            valores.push(valor);
+                        }
+                    }
+                }
+                for (const prop of metadata.columnas) {
+                    if (metadata.ids.includes(prop)) {
+                        const valor = this[prop];
+                        if (valor !== undefined && valor !== null) {
+                            valores.push(valor);
+                        }
+                    }
+                }
+                return valores;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        
+        
+
+        target.set = function(data: any) {
+            const instanceMetadata = getMetadata(this.constructor);
+            for (const prop in data) {
+                if (instanceMetadata.columnas.includes(prop)) {
+                    this[prop] = data[prop];
+                } else {
+                    console.log(`La propiedad "${prop}" no es una columna válida en la clase "${className}"`);
+                }
+            }
+        };    
+    };
+}
