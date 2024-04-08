71b3bc6cba5c57950ffeb21d393d0d5ac17b2ac4
DDelvRoj <ddelvallerojas@gmail.com>
2024-04-08 10:55:03 -0400

    Reestructuración de UsuarioService para el uso de los modelos y la nueva clase independiente de conexión.

--- vino-vault-server/src/service/usuarioService.ts
+++ vino-vault-server/src/service/usuarioService.ts
@@ -1,42 +1,55 @@
+import { ConexionDataBase } from "../model/conexionBD";
 import { UsuarioModel } from "../model/usuarioModel";
-import { Credential } from "../type";
+import { Usuario } from "../type";
+import bcryptUtil from "../util/bcryptUtil";
+import { transformarTexto } from "../util/trasformarTextoUtil";
 
 export class UsuarioService {
+    
+    private usuario:UsuarioModel;
 
-    usuario:UsuarioModel;
-
-    constructor(usuarioExistenteCredential:Credential){
-        this.usuario = new UsuarioModel(usuarioExistenteCredential);
+    constructor(conexion:ConexionDataBase){
+        this.usuario = new UsuarioModel(conexion, this);
     }
 
-    async buscarUsuarioPorNombre (userName:string)  {
+    async buscarUsuario (usuario:Usuario)  {
         try {
-            const usuarioValor = await this.usuario.obtenerUsuarioPorUsername(userName);
+            const usuarioValor = await this.usuario.buscar(usuario);
             return usuarioValor;
         } catch (error) {
             throw error;
         }
     }
 
-    async crearUsuario(usuarioNuevo:Credential) {
+    
+    async coindicenDatos (nombre:Usuario)  {
         try {
-            await this.crearUsuario(usuarioNuevo);
+            const usuarios:Usuario[] = await this.usuario.listar();
+            const usuario:Usuario = await usuarios.map(async u => {
+                const usuarioValidacion = await bcryptUtil.desencriptarYCompararData(transformarTexto(nombre.usuario), u.usuario);
+                const claveValidacion = await bcryptUtil.desencriptarYCompararData(transformarTexto(nombre.clave),u.clave);   
+                if(usuarioValidacion && claveValidacion){
+                    return u;
+                }
+            })[0];
+            if(usuario){
+                return usuario;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async crearUsuario(usuarioNuevo:Usuario) {
+        try {
+            await this.usuario.insertar(usuarioNuevo);
         } catch (error) {
             throw error;
         }
     }
 
-    async conectar(){
+    async validarUsuario(usuarioValidable:Usuario){
         try {
-            await this.usuario.conectar();
-        } catch (error) {
-            throw error;   
-        }
-    }
-    
-    async desconectar(){
-        try {
-            await this.usuario.desconectar();
+           await this.usuario.validarUsuarios(usuarioValidable);
         } catch (error) {
             throw error;
         }
