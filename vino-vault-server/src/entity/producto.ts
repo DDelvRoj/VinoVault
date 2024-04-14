f080a92866e803d72f6514634781a9c3743c13f0
DDelvRoj <ddelvallerojas@gmail.com>
2024-04-13 20:30:52 -0400

    Creación de entidad Producto.

---
+++ vino-vault-server/src/entity/producto.ts
@@ -0,0 +1,93 @@
+import { Column, Entity } from "../decorator/tableDecorator";
+import { Producto as pro } from "../type";
+
+@Entity('producto')
+export class Producto {
+    private _id_producto:string;
+    private _ean:string;
+    private _cantidad:number;
+    private _descripcion:string;
+    private _marca:string;
+    private _nombre_producto:string;
+    private _precio:number;
+    
+    @Column({esId:true, esUUID:true})
+    public get id_producto() : string {
+        return this._id_producto.toString();
+    }
+    
+    public set id_producto(v : string) {
+        this._id_producto = v;
+    }
+    
+    @Column({esId:true})
+    public get ean() : string {
+        return this._ean;
+    }
+    
+    
+    public set ean(v : string) {
+        this._ean = v;
+    }
+
+    @Column()
+    public get cantidad() : number {
+        return this._cantidad;
+    }
+    
+    public set cantidad(v : number) {
+        this._cantidad = v;
+    }
+
+    @Column()
+    public get descripcion() : string {
+        return this._descripcion;
+    }
+
+    
+    public set descripcion(v : string) {
+        this._descripcion = v;
+    }
+
+    @Column()
+    public get marca() : string {
+        return this._marca;
+    }
+    
+    
+    public set marca(v : string) {
+        this._marca = v;
+    }
+    
+    @Column()
+    public get nombre_producto() : string {
+        return this._nombre_producto;
+    }
+    
+    
+    public set nombre_producto(v : string) {
+        this._nombre_producto = v;
+    }
+    
+    @Column()
+    public get precio() : number {
+        return this._precio;
+    }
+    
+    
+    public set precio(v : number) {
+        this._precio = v;
+    }
+    
+    constructor(pro?:pro){
+        if(pro){
+            this._cantidad=pro.cantidad;
+            this._descripcion=pro.descripcion;
+            this._ean=pro.ean;
+            this._id_producto=pro.ean;
+            this._marca=pro.marca;
+            this._nombre_producto=pro.nombre_producto;
+            this._precio=pro.precio;
+        }
+    }
+}
