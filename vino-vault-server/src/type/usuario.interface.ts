648603c52d377b8cce982f053b93be7a269bb46c
DDelvRoj <ddelvallerojas@gmail.com>
2024-05-25 23:45:36 -0400

    Inserción de nuevo campo llamado pseudoclave, que permite tener una clave temporal para usuarios que no están registrados de manera oficial en la base de datos.

--- vino-vault-server/src/type/usuario.interface.ts
+++ vino-vault-server/src/type/usuario.interface.ts
@@ -2,6 +2,7 @@ export interface Usuario {
     id_usuario?:string,
     nombre?:string,
     usuario?:string, 
+    pseudoclave?:string,
     clave?:string,
     admin?:boolean,
     creado?:boolean
