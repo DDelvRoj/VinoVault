00c72745afe82c5b5cb7ae3b77bfa0d2dc59d97e
DDelvRoj <ddelvallerojas@gmail.com>
2024-06-03 09:34:05 -0400

    Se movió funciones pertenecientes a RegistroController, dado a que ambos trabajan en la ruta de personas, y corrección de detalles menores.

--- vino-vault-server/src/controller/personasController.ts
+++ vino-vault-server/src/controller/personasController.ts
@@ -5,6 +5,8 @@ import { visorSesion } from "../util/sesionUtil";
 import { UsuarioService } from '../service/usuarioService';
 import { Usuario } from "../entity/usuario";
 import { getConexionCargada } from "../util/conexionUtil";
+import bcryptUtil from "../util/bcryptUtil";
+import { transformarTexto } from "../util/transformarTextoUtil";
 
 const personasRouter:Router = Router();
 
@@ -20,7 +22,7 @@ personasRouter.get('/personas', authenticateToken, async (req:Request, res:Respo
         res.status(201).json(resultado)
     } catch (error) {
         console.log(error);
-        res.status(500).json({error:'Error'})
+        res.status(500).json({error:`Error al realizar la carga de productos: ${error}`})
     }
 })
 
@@ -37,18 +39,55 @@ personasRouter.post('/personas/validar/:id',authenticateToken , async (req:Reque
         await conexion.desconectar();
         conexion = getConexionCargada(req);
         await conexion.conectar();
-        //await (new UsuarioService(conexion)).crearUsuario(usuarioBusqueda);
-        usuarioBusqueda.creado=true;
+        await (new UsuarioService(conexion)).crearUsuario(usuarioBusqueda);
+        /* Eliminamos la pseudoclave, imagina que este es y no a la vez la contraseña,
+        *  lo que ocurre es que esta nos sirve para transformar la contraseña, despues encriptarla,
+        * entonces la única manera de iniciar sesión es a traves de este servidor y no directamente desde,
+        * la base de datos. 
+        */
         usuarioBusqueda.pseudoclave=null;
-        console.log(usuarioBusqueda);
-        
+        usuarioBusqueda.creado=true;
         await (new UsuarioService(conexion)).modificarUsuario(new Usuario(usuarioBusqueda));
         
         res.status(201).json({message:'Validación Éxitosa...'});
     } catch (error) {
         console.log(error);
-        res.status(500).json({error:'Error'})
+        res.status(500).json({error:`Error al realizar la validación: ${error}`})
     }  
 })
 
+
+personasRouter.post('/personas/registrar', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  const conexion: ConexionDataBase = new ConexionDataBase(visorSesion);
+  const usuarioBuscador: UsuarioService = new UsuarioService(conexion);
+  try {
+    await conexion.conectar();
+    // Verificar si el usuario ya existe en la base de datos
+    const existeElUsuario = await usuarioBuscador.buscarUsuario(new Usuario({ usuario: username }));
+    console.log("Buscando usuario...");
+    if (existeElUsuario.id_usuario != null) {
+      res.status(400).json({ error: 'El usuario ya existe' });
+      return;
+    }
+    console.log("Creando usuario...");
+
+    await usuarioBuscador.insertarUsuario(new Usuario({
+        nombre: await bcryptUtil.encriptarData(transformarTexto(username)),
+        admin: false,
+        usuario: username,
+        pseudoclave: password,
+        clave: await bcryptUtil.encriptarData(transformarTexto(password)),
+        creado: false
+      }));
+
+    res.status(201).json({ message: 'Usuario registrado correctamente' });
+  } catch (err) {
+    console.error('Error al registrar usuario en Cassandra', err);
+    res.status(500).json({ error: `Error al registrar usuario en Cassandra: ${err}` });
+  }
+  await conexion.desconectar();
+}
+);
 export default personasRouter;
