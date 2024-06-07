14d934fb2a3e7d3f9101500a6332302fa560c91e
EladioGimenez <gimenezaparisf@gmail.com>
2024-06-06 22:55:12 -0400

    Creación de un login básico pero que consume al AutenticacionProvider.

---
+++ vino-vault/src/pages/Login.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { useAutenticacion } from '../contexts/AutenticacionContext';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonAlert } from '@ionic/react';
+import './Login.css';
+import { TokenStore } from '../data/TokenStore';
+import { useHistory } from 'react-router';
+
+const Login: React.FC = () => {
+    const token = TokenStore.useState(s=>s.token);
+    const [usuario, setUsuario] = useState<string|null>(null);
+    const [clave, setClave] = useState<string|null>(null);
+    const [camposCompletos, setCamposCompletos] = useState(false);
+    const { login } = useAutenticacion();
+    const [error, setError] = useState<string | null>(null);
+    const history = useHistory();
+    useEffect(()=>{
+        if(token){
+            history.push("/");
+        }
+    },[token])
+
+    const handleLogin = async (event: any) => {
+        event.preventDefault();
+        setError(null);
+        try {
+            if( usuario && clave){
+                await login(usuario, clave);
+                return;
+            }
+            setError('Hay valores vacíos')
+        } catch (error) {
+            console.log(error);
+            
+            setError('Login fallido');
+        }
+    };
+
+    const handleUsuario = (event: CustomEvent) => {
+        const value = event.detail.value!;
+        setUsuario(value);
+        evaluarCampos();
+    };
+    const handleClave = (event: CustomEvent) => {
+        const value = event.detail.value!;
+        setClave(value);
+        evaluarCampos();
+    };
+
+    const evaluarCampos = ()=>{
+        const val = !(!usuario || !clave);
+        setCamposCompletos(val);
+    }
+
+  return (
+    <IonPage>
+        <IonHeader>
+            <IonToolbar>
+            <IonTitle>Iniciar Sesión</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent>
+        <div className="login-wrapper">
+            
+            <div className="brand">
+               Login
+            </div>
+
+            <IonItem>
+              <IonInput value={usuario} placeholder="Usuario" required onIonInput={handleUsuario}></IonInput>
+            </IonItem>
+
+            <IonItem>
+              <IonInput value={clave} placeholder="Clave" required type="password" onIonInput={handleClave}></IonInput>
+            </IonItem>
+            <IonButton expand="full" color="dark" onClick={handleLogin} disabled={!camposCompletos} >
+              Iniciar Sesión
+            </IonButton>
+
+        </div>
+        {error && <IonAlert isOpen={true} header={'Error'} message={error} buttons={['OK']} />}
+        </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
