d87fe511cb3a6144fe10a88f350503e7ef248f35
EladioGimenez <gimenezaparisf@gmail.com>
2024-06-06 22:56:14 -0400

    Se creó distintas funciones con la capacidad de consumir al servidor, aún está en etapa de prueba.

--- vino-vault/src/data/fetcher.ts
+++ vino-vault/src/data/fetcher.ts
@@ -7,8 +7,8 @@ export const fetchData = async () =>{
   ProductStore.update(s=>{s.products =  [...products]});
 }
 
-const fetchProducts = async (category: string): Promise<Product[]> => {
-  const response = await fetch(`products/${category}`);
+const fetchProducts = async (id: string): Promise<Product[]> => {
+  const response = await fetch(`products/${id}`);
   const data: Product[] = await response.json();
 
   // Set a product id
@@ -18,3 +18,69 @@ const fetchProducts = async (category: string): Promise<Product[]> => {
 
   return data;
 }
+
+
+interface FetchConfig {
+  method: string;
+  headers: HeadersInit;
+  body?: string;
+}
+
+const apiFetch = async <T>(
+  ruta: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  headers?: HeadersInit,
+  body?: Record<string, any>
+): Promise<T> => {
+  const url = `link/${ruta}`;
+  const defaultHeaders: HeadersInit = {
+    'Content-Type': 'application/json',
+    ...headers,
+  };
+
+  const config: FetchConfig = {
+    method: method,
+    headers: defaultHeaders,
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  try {
+    const response = await fetch(url, config);
+    if (!response.ok) {
+      const errorMessage = `Error: ${response.statusText}`;
+      throw new Error(errorMessage);
+    }
+    return (await response.json()) as T;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
+};
+
+const authHeader = async ()=>{
+  const token = localStorage.getItem('token');
+  const authHeader:HeadersInit = {
+    'Authorization':`Bearer ${token}`
+  }
+  return authHeader
+}
+
+export const login = async (username:string, password:string) => {
+  const body = { username, password };
+  return apiFetch<string>('login', 'POST', undefined, body);
+};
+
+export const fetchPersonas = async () => { 
+  return apiFetch('personas','GET', await authHeader());
+};
+
+export const fetchProductos = async () => {
+  return apiFetch<Product[]>('productos','GET', await authHeader());
+};
+
+export const fetchProductById = async (id:string) => {
+  return apiFetch<Product>( `/${id}`,'GET', await authHeader());
+};
