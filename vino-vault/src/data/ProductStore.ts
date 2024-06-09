d4f98e4c176cdc59e58b6c537f89b223e62a1b35
David Delvalle <gimenezaparisf@gmail.com>
2024-06-09 17:19:44 -0400

    Eliminamos tipados que ya no nos sirven, y también realizamos una carga inicial en caso de que exista algún valor guardado en la memoria local.

--- vino-vault/src/data/ProductStore.ts
+++ vino-vault/src/data/ProductStore.ts
@@ -1,18 +1,13 @@
 import { Store } from "pullstate";
+import { Producto } from "./types";
 
-interface Product {
-  id: number;
-  image: string;
-  price: string;
-  name: string;
-  // Otros campos que pueda tener un producto
-}
+
 interface ProductStoreState{
-  products:Product[]
+  products:Producto[]
 }
 
 
 
 export const ProductStore = new Store<ProductStoreState>({
-  products: []
+  products: (JSON.parse(localStorage.getItem('productos')??'[]') as Producto[]) 
 });
