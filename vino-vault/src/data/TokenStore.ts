ac651b50591008409f20bea0875187496ab39e41
David Delvalle <gimenezaparisf@gmail.com>
2024-06-09 17:15:14 -0400

    Dado que la función de TokenStore para guardar solo sé utiliza una vez, lo movemos a AutenticacionProvider.

--- vino-vault/src/data/TokenStore.ts
+++ vino-vault/src/data/TokenStore.ts
@@ -7,9 +7,3 @@ interface TokenStoreState {
 export const TokenStore = new Store<TokenStoreState>({
     token:localStorage.getItem('token')?.toString()
 });
-
-export const guardarToken = (token:string|null)=>{
-    TokenStore.update(s=>{
-        s.token=token
-    });
-}
