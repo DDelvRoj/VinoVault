0ab01f86a4f423b1b658b934a76282a21f104ccf
David Delvalle Rojas <ddelvallerojas9@gmail.com>
2024-06-23 18:18:44 -0400

    Eliminación de rutas innecesarias, y creación de nueva ruta para la creación de usuarios, de manera de respectar y crear el CRUD sin repeticiones de funciones.

--- vino-vault-server/src/controller/personasController.ts
+++ vino-vault-server/src/controller/personasController.ts
@@ -4,11 +4,13 @@ import { ConexionDataBase } from '../model/conexionBD';
 import { visorSesion } from "../util/sesionUtil";
 import { UsuarioService } from '../service/usuarioService';
 import { Usuario } from "../entity/usuario";
+import { Usuario as UsuarioType } from "../type";
 import { getConexionCargada } from "../util/conexionUtil";
 import bcryptUtil from "../util/bcryptUtil";
 import { transformarTexto } from "../util/transformarTextoUtil";
 
 const personasRouter:Router = Router();
+
 
 personasRouter.get('/personas', authenticateToken, async (req:Request, res:Response)=>{
     const conexion:ConexionDataBase = getConexionCargada(req);
@@ -22,75 +24,57 @@ personasRouter.get('/personas', authenticateToken, async (req:Request, res:Respo
         });
         
         await conexion.desconectar();
-        res.status(201).json(resultado)
+        res.status(200).json(resultado)
     } catch (error) {
         console.log(error);
         res.status(500).json({error:`Error al realizar la carga de productos: ${error}`})
     }
 })
 
-personasRouter.post('/personas/validar/:id',authenticateToken , async (req:Request, res:Response)=>{
-    const usuarioMod:Usuario = new Usuario({id_usuario:req.params['id']});
-    let conexion:ConexionDataBase = new ConexionDataBase(visorSesion);
-    try {
-        await conexion.conectar();
-        const usuarioBusqueda:Usuario = await (new UsuarioService(conexion)).buscarUsuario(usuarioMod);
-        if(usuarioBusqueda === undefined || usuarioBusqueda === null){
-            res.status(401).json({error:'El usuario no está registrado en la base de datos.'})
-        }
-        
-        await conexion.desconectar();
-        conexion = getConexionCargada(req);
-        await conexion.conectar();
-        await (new UsuarioService(conexion)).crearUsuario(usuarioBusqueda);
-        /* Eliminamos la pseudoclave, imagina que este es y no a la vez la contraseña,
-        *  lo que ocurre es que esta nos sirve para transformar la contraseña, despues encriptarla,
-        * entonces la única manera de iniciar sesión es a traves de este servidor y no directamente desde,
-        * la base de datos. 
-        */
-        usuarioBusqueda.pseudoclave=null;
-        usuarioBusqueda.creado=true;
-        await (new UsuarioService(conexion)).modificarUsuario(new Usuario(usuarioBusqueda));
-        
-        res.status(201).json({message:'Validación Éxitosa...'});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:`Error al realizar la validación: ${error}`})
-    }  
-})
 
+personasRouter.put('/personas',authenticateToken , async (req: Request, res: Response) => {
+  const conexion: ConexionDataBase = getConexionCargada(req);
+  const usuarioNuevo:UsuarioType = req.body;
+  console.log(usuarioNuevo);
 
-personasRouter.post('/personas/registrar', async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  console.log(username, password);
-  const conexion: ConexionDataBase = new ConexionDataBase(visorSesion);
-  const usuarioBuscador: UsuarioService = new UsuarioService(conexion);
   try {
+    
     await conexion.conectar();
     // Verificar si el usuario ya existe en la base de datos
-    const existeElUsuario = await usuarioBuscador.buscarUsuario(new Usuario({ usuario: username }));
-    console.log("Buscando usuario...");
-    if (existeElUsuario.id_usuario != null) {
+    
+    if ((await  new UsuarioService(conexion).buscarUsuario(new Usuario({ usuario: usuarioNuevo.usuario }))).id_usuario != null) {
       res.status(400).json({ error: 'El usuario ya existe' });
       return;
     }
-    console.log("Creando usuario...");
+    const usOb = new Usuario({
+      nombre: await bcryptUtil.encriptarData(transformarTexto(usuarioNuevo.usuario)),
+      admin: usuarioNuevo.admin,
+      usuario: usuarioNuevo.usuario,
+      pseudoclave: usuarioNuevo.pseudoclave,
+      clave: await bcryptUtil.encriptarData(transformarTexto(usuarioNuevo.pseudoclave)),
+      creado: false
+    });
+    console.log(usOb);
+    
 
-    await usuarioBuscador.insertarUsuario(new Usuario({
-        nombre: await bcryptUtil.encriptarData(transformarTexto(username)),
-        admin: false,
-        usuario: username,
-        pseudoclave: password,
-        clave: await bcryptUtil.encriptarData(transformarTexto(password)),
-        creado: false
-      }));
+    await new UsuarioService(conexion).insertarUsuario(usOb);
+    console.log('Usuario creado, buscando su id...');
+    
+    const usuarioConID = await  new UsuarioService(conexion).buscarUsuario(new Usuario({ usuario: usuarioNuevo.usuario }));
+    await new UsuarioService(conexion).crearUsuario( usuarioConID );
+    console.log(`Id encontrado ${usuarioConID.id_usuario}, dandole un rol...`);
+    
+    await new UsuarioService(conexion).cambiarRolUsuario(usuarioConID);
 
+    
     res.status(201).json({ message: 'Usuario registrado correctamente' });
   } catch (err) {
     console.error('Error al registrar usuario en Cassandra', err);
     res.status(500).json({ error: `Error al registrar usuario en Cassandra: ${err}` });
+  } finally {
+    await conexion.desconectar();
   }
-  await conexion.desconectar();
+  
 }
 );
 export default personasRouter;
