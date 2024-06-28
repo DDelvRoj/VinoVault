e1f79cbc297f83e13ade462896b0f9552925ecc3
David Delvalle Rojas <ddelvallerojas9@gmail.com>
2024-06-28 18:01:19 -0400

    Eliminación de parametros en desuso.

--- vino-vault/src/data/types.ts
+++ vino-vault/src/data/types.ts
@@ -3,17 +3,16 @@ export interface Producto {
   id_producto?: string,
   ean?:string,
   nombre_producto?:string,
-  cantidad:number|0,
+  cantidad?:number|0,
   imagen?:string,
   descripcion?:string,
   marca?:string,
-  precio:number|0
+  precio?:number|0
 }
 
 export interface Persona {
   id_usuario?:string,
   usuario?: string,
   pseudoclave?: string,
-  creado:boolean,
   admin:boolean
 }
